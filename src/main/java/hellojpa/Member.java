9b165124923a5e1c2bde8864f1484f3c5cf9a86b
soyeon <luora@naver.com>
2023-08-21 18:28:11 +0900

    [add] @Table [#10]

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,8 +2,10 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "MBR") // DB의 MBR 이라는 테이블이랑 매핑
 public class Member {
     @Id
     private Long id;
