2d47c8d77e33b7aa59592c5f0e16327c8f881e94
soyeon <luora@naver.com>
2023-08-23 21:19:33 +0900

    [add] LocalDate, LocalDateTime [#14]

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
@@ -26,6 +28,10 @@ public class Member {
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
 
     @Lob
     private String description;
