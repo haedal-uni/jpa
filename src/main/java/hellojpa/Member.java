3228eeffe27eedb132e7086c148ef9a4114a0f75
soyeon <luora@naver.com>
2023-08-24 18:42:55 +0900

    [add] SEQUENCE 전략 [#16]

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,10 +3,11 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    private Long id;
 
     @Column(name = "name", nullable = false)
     private String username;
@@ -14,11 +15,11 @@ public class Member {
     public Member(){
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
