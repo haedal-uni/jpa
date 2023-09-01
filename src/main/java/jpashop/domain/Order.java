5b19b9be8d19f42970e424d73b169545866de337
soyeon <luora@naver.com>
2023-09-01 20:51:14 +0900

    [add] Member - Order 양방향 매핑 [#26]

--- src/main/java/jpashop/domain/Order.java
+++ src/main/java/jpashop/domain/Order.java
@@ -1,54 +1,60 @@
-//package jpashop.domain;
-//
-//import javax.persistence.*;
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Table(name = "ORDERS") // DB마다 다르긴 한데 ORDER가 예약어로 걸려있다.(order by) 그래서 orders로 많이 쓴다.
-//public class Order {
-//    @Id @GeneratedValue
-//    @Column(name = "ORDER_ID")
-//    private Long id;
-//
+package jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS") // DB마다 다르긴 한데 ORDER가 예약어로 걸려있다.(order by) 그래서 orders로 많이 쓴다.
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
 //    @Column(name = "MEMBER_ID")
 //    private Long memberId;
-//
-//    //private Member member;
-//
-//    private LocalDateTime orderDate;
-//
-//    @Enumerated(EnumType.STRING)
-//    private OrderStatus status;
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public Long getMemberId() {
-//        return memberId;
-//    }
-//
-//    public void setMemberId(Long memberId) {
-//        this.memberId = memberId;
-//    }
-//
-//    public LocalDateTime getOrderDate() {
-//        return orderDate;
-//    }
-//
-//    public void setOrderDate(LocalDateTime orderDate) {
-//        this.orderDate = orderDate;
-//    }
-//
-//    public OrderStatus getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(OrderStatus status) {
-//        this.status = status;
-//    }
-//}
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+    /*
+    양방향 매핑을 하고 싶으면 Member에서 컬렉션을 넣으면 된다.
+    가급적 단방향으로 한다.
+     */
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
