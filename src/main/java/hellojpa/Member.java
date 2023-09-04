39a07ad729b940a951c061cc663f3d09ddd38fb9
soyeon <luora@naver.com>
2023-09-04 22:28:26 +0900

    [add] 일대다 단방향 매핑 [#28]

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -9,15 +9,6 @@ public class Member {
     private Long id;
     @Column(name = "USERNAME")
     private String username;
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
-    /*
-    Member랑 Team은 reference로 가져가야 하는데 db에 맞춰서 모델링을 하고 있다.
-     */
-
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
 
     public Long getId() {
         return id;
@@ -33,14 +24,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void changeTeam(Team team) {
-        this.team = team;
-        team.getMembers().add(this);
     }
 }
