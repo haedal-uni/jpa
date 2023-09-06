acd81f1fc5b1b7838019a27f17e1c12e749b05f8
soyeon <luora@naver.com>
2023-09-06 17:56:48 +0900

    [add] 다대다 양방향 매핑 [#32]

--- src/main/java/hellojpa/Product.java
+++ src/main/java/hellojpa/Product.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Product {
@@ -12,6 +11,9 @@ public class Product {
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
