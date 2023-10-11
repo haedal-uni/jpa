a30374f2a1eca6e100f38463ea819ce7dbadc8b9
soyeon <luora@naver.com>
2023-10-11 17:58:14 +0900

    [add] 값 타입 공유 참조 [#52]

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,12 +14,19 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "zipcode"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "zipcode");
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(address);
             em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setHomeAddress(address);
+            em.persist(member2);
+
+            member.getHomeAddress().setCity("newCity");
 
             tx.commit();
         }catch (Exception e){
