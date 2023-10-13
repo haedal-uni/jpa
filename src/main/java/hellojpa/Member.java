bd3fb345c850f9482ce6c52dea261c7087389e42
soyeon <luora@naver.com>
2023-10-14 00:01:49 +0900

    [add] 값 타입 컬렉션 [#56]

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,11 @@
 package hellojpa;
 
 import javax.persistence.*;
+import javax.print.attribute.standard.MediaSize;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member{
@@ -10,13 +15,18 @@ public class Member{
     @Column(name = "USERNAME")
     private String username;
 
-    // 기간 Period
-    @Embedded
-    private Period workPeriod;
-
     // 주소
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID")) // 테이블 명 지정
+    @Column(name = "FOOD_NAME") // String 하나이고 정의한게 아니라서 작성함
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection // 값 타입 컬렉션
+    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 
     public Address getHomeAddress() {
         return homeAddress;
@@ -36,10 +46,6 @@ public class Member{
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
     }
 
     public void setHomeAddress(Address homeAddress) {
