3729a60fcb25bc536d8c441deb22904d54ee1545
soyeon <luora@naver.com>
2023-10-19 14:19:16 +0900

    [add] 주석 제거 [#58]

--- src/main/java/jpashop/domain/Delivery.java
+++ src/main/java/jpashop/domain/Delivery.java
@@ -1,19 +1,19 @@
-//package jpashop.domain;
-//
-//import javax.persistence.*;
-//
-//import static javax.persistence.FetchType.LAZY;
-//
-//@Entity
-//public class Delivery extends BaseEntity{
-//    @Id @GeneratedValue
-//    private Long id;
-//
-//    private String city;
-//    private String street;
-//    private String zipcode;
-//    private DeliveryStatus status;
-//
-//    @OneToOne(mappedBy = "delivery", fetch = LAZY)
-//    private Order order; // 1대 1 양방향
-//}
+package jpashop.domain;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+public class Delivery extends BaseEntity{
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order; // 1대 1 양방향
+}
