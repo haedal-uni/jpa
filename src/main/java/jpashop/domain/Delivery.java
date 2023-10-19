bd695ee2902d8454fe138d9b69bd8dbadee45cb1
soyeon <luora@naver.com>
2023-10-20 03:48:15 +0900

    [add] 값 타입 매핑 [#58]

--- src/main/java/jpashop/domain/Delivery.java
+++ src/main/java/jpashop/domain/Delivery.java
@@ -9,11 +9,43 @@ public class Delivery extends BaseEntity{
     @Id @GeneratedValue
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
+
     private DeliveryStatus status;
 
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order; // 1대 1 양방향
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
