8c7b8d7b6b4b22b860b4f22dbfe84a4e9c070513
soyeon <luora@naver.com>
2023-10-28 18:43:11 +0900

    [add] 파라미터 바인딩 - 이름 기준 [#62]

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -16,17 +16,12 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(10);
+            em.persist(member);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-            List<Member> resultList = query.getResultList();
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
-
-            // 반환 값이 딱 한개일 때
-            TypedQuery<Member> query2 = em.createQuery("select m from Member m where m.id=10", Member.class);
-            Member result = query2.getSingleResult();
-            System.out.println("result = " + result);
+            Member result = em.createQuery("select m from Member m where m.username =:username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println("result = " + result.getUsername());
 
             tx.commit();
         }catch (Exception e){
