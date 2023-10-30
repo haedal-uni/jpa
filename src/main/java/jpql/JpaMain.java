9b81043cfcf0fdacc6d06790f46695483890550b
soyeon <luora@naver.com>
2023-10-30 23:44:27 +0900

    [add] Query 타입으로 조회 [#64]

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -21,8 +21,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            em.createQuery("select distinct m.username, m.age from Member m")
+            List resultList = em.createQuery("select distinct m.username, m.age from Member m")
                     .getResultList();
+
+            Object o = resultList.get(0);
+            Object[] result = (Object[]) o;
+
+            System.out.println("username = " + result[0]);
+            System.out.println("age = " + result[1]);
 
             tx.commit();
         }catch (Exception e){
